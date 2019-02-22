5fbcabbd793353de5fe0072baa92ce32c357a2db
Saurav Shrestha <sauravspursstha@gmail.com>
2019-02-22 09:43:26 +0545

    added switch condition

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -10,8 +10,32 @@ namespace GitTest
     {
         static void Main(string[] args)
         {
-            
+            string name = EnterName();
 
+            switch (name)
+            {
+                case "Saurav":
+                    Console.WriteLine("hello");
+                    break;
+                case "Aakriti":
+                    Console.WriteLine("hello");
+                    break;
+                case "Rosan":
+                    Console.WriteLine("hello");
+                    break;
+                case "Samiksha":
+                    Console.WriteLine("hello");
+                    break;
+            }
+
+        }
+
+        public static string EnterName()
+        {
+            Console.WriteLine("Enter your name:");
+            string name = Console.Read().ToString();
+
+            return name;
         }
     }
 }
